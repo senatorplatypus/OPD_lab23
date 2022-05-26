124ec0987d6b9c828facf55977455b72d0dae295
Danil <danil-skopin@rambler.ru>
2022-05-26 22:31:32 +0300

    add feature 2

---
+++ lab1_task2/Countdown.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_task2
+{
+    public class Countdown
+    {
+        private List<IEventSubscribers> subscribers;
+
+        public Countdown()
+        {
+            subscribers = new List<IEventSubscribers>();
+        }
+
+        public List<IEventSubscribers> Subscribers
+        {
+            
+            get {return subscribers; }
+        }
+
+        public void Subscription(IEventSubscribers subscriber)
+        {
+            subscribers.Add(subscriber);
+        }
+
+        public void Unsubscription(IEventSubscribers subscriber)
+        {
+            subscribers.Remove(subscriber);
+        }
+
+        public void SubNotification(int time)
+        {
+            foreach (IEventSubscribers subscriber in subscribers)
+            {
+                Thread.Sleep(time);
+                Console.WriteLine("Time delay " + time.ToString() + " ms");
+                subscriber.SubMessage("Time out!");
+            }
+        }
+
+         
+    }
+
+
+}
