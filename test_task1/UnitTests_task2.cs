23d8552d544b683e08114ba9194df40396a3d6a1
Danil <danil-skopin@rambler.ru>
2022-05-26 22:33:35 +0300

    add feature 3

---
+++ test_task1/UnitTests_task2.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace lab1_task2
+{
+    public class UnitTests_task2
+    {
+        [Test]
+        public void Subscriber1Login()
+        {
+            Subscriber1 subscriber = new Subscriber1("Danil");
+            string login = subscriber.Login;
+
+            string res = "Danil";
+
+            Assert.AreEqual(res, login);
+        }
+
+        [Test]
+        public void Subscriber2Login()
+        {
+            Subscriber2 subscriber = new Subscriber2("Danil");
+            string login = subscriber.Login;
+
+            string res = "Danil";
+
+            Assert.AreEqual(res, login);
+        }
+
+        [Test]
+        public void Subscriber3Login()
+        {
+            Subscriber3 subscriber = new Subscriber3("Danil");
+            string login = subscriber.Login;
+
+            string res = "Danil";
+
+            Assert.AreEqual(res, login);
+        }
+    }
+}
